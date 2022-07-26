a15441473f457a4e758bf1513ff6357ceb7a67d4
Hossein Ahmadi <hossein.ahmadi@veriff.net>
2022-07-26 15:00:49 +0200

    feat: primary color added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss';
 
 const App = () => {
-  return <div className="text-blue-500">This is going to be my personal website!</div>;
+  return <div className="text-primary-700">This is going to be my personal website!</div>;
 };
 
 export default App;
