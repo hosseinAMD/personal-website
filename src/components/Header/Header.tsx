516a51da52a12573daf1bf9ab5819b1eef3b322c
Hossein Ahmadi <hossein.ahmadi@veriff.net>
2022-07-29 13:18:35 +0200

    feat: dark mode and favicon added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import reactLogo from 'assets/react.svg';
 
 const Header = () => (
-  <nav className="w-full py-4 px-2 shadow-md">
+  <nav className="w-full py-4 px-2 shadow-md dark:shadow-gray-700 dark:shadow-sm">
     <div className="container mx-auto flex justify-between items-center">
       <span className="flex flex-col md:flex-row md:items-end ">
         <span className="text-4xl font-medium mb-1 md:mb-0 md:mr-3">Welcome</span>
