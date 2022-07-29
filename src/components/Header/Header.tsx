8fe975d36158237ec715757bce8d903c9cf0fe34
Hossein Ahmadi <hossein.ahmadi@veriff.net>
2022-07-29 16:32:30 +0200

    feat: header style updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => (
   <nav className="w-full py-4 px-2 shadow-md dark:shadow-gray-700 dark:shadow-sm">
     <div className="container mx-auto flex justify-between items-center">
       <span className="flex flex-col md:flex-row md:items-end ">
-        <span className="text-4xl font-medium mb-1 md:mb-0 md:mr-3">Welcome</span>
+        <span className="text-4xl font-medium mb-1 md:mb-0 md:mr-3">Hello &#128075;</span>
         <span className="font-mono text-base block overflow-hidden border-r-2 border-secondary-600 whitespace-nowrap pr-1 animate-typewriter ">
           I&apos;m Hossein Ahmadi
         </span>
