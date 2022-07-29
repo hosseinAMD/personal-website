9846576f6914e0f896c6306412f01ed98ced4c47
Hossein Ahmadi <hossein.ahmadi@veriff.net>
2022-07-29 17:03:17 +0200

    feat: about mesection added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Articles from 'containers/Articles/Articles';
 import Header from 'components/Header/Header';
 import AppContaienr from 'components/Layout/AppContainer';
+import AboutMe from 'containers/AboutMe/AboutMe';
 
 const App = () => {
   return (
     <>
       <Header />
       <AppContaienr>
+        <AboutMe />
         <Articles />
       </AppContaienr>
     </>
---
+++ src/containers/AboutMe/AboutMe.tsx
@@ -0,0 +1,19 @@
+import Section from 'components/Layout/Section';
+import { FunctionComponent } from 'react';
+
+const AboutMe: FunctionComponent = () => (
+  <Section title="About Me">
+    <p>
+      I&apos;m Hossein Ahmadi! I am a Front End Engineer at <a href="veriff.me">Veriff</a>. I have
+      strong experience of working with React + TypeScript and I love making beautiful UI and
+      deliver fancy products.
+    </p>
+    <p>
+      I live in Barcelona, Spain &#127773;.Writing techniacl articles and helping junior developers
+      to improve are my hobbies in the world of developement. Thank you for visiting my website!
+      &#127839;
+    </p>
+  </Section>
+);
+
+export default AboutMe;
